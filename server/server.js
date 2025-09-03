6a9e8577f1c5ebb829d40aa0a3fea0c7f02fe8ea
Jan Dyląg <jan.dylag@gmail.com>
2025-09-03 20:46:47 +0200

    initializing backend

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to the Socio API!'});
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Database connected successfully!'))
+.catch((err) => console.error('Database connection failed: ', err));
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
